66db78422e46e2eb5968b38651786f624cfecb58
MrZWH <1875290692@qq.com>
2018-07-15 01:11:56 +0800

    store 相关代码添加热更替功能

--- client/store/store.js
+++ client/store/store.js
@@ -7,10 +7,18 @@ import getters from './getters/getters'
 const isDev = process.env.NODE_ENV === 'development'
 
 export default () => {
-  return new Vuex.Store({
+  const store = new Vuex.Store({
     strict: isDev, // 只在开发环境中使用，规范vuex store 中数据的修改
     state: defaultState,
     mutations,
     getters
   })
+
+  if (module.hot) {
+    module.hot.accept([], () => {
+
+      store.hotUpdate({})
+    })
+  }
+  return store
 }
