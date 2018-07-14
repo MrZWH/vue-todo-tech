147f85093e2b6156e30ca9c15823382055843c39
MrZWH <1875290692@qq.com>
2018-07-15 00:47:56 +0800

    Vuex之mutation和action

---
+++ client/store/actions/actions.js
@@ -0,0 +1,6 @@
+// 处理异步修改数据的方法
+export default {
+  updateCountSync (store, data) {
+
+  }
+}
--- client/store/store.js
+++ client/store/store.js
@@ -4,8 +4,11 @@ import defaultState from './state/state'
 import mutations from './mutations/mutations'
 import getters from './getters/getters'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export default () => {
   return new Vuex.Store({
+    strict: isDev, // 只在开发环境中使用，规范vuex store 中数据的修改
     state: defaultState,
     mutations,
     getters
